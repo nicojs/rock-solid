913d856ccab65e8f1d85e57e3a0c6e3262e09baf
Nico Jansen <jansennico@gmail.com>
2023-07-28 12:26:42 +0200

    Fix lint

--- packages/backend/src/reports.controller.ts
+++ packages/backend/src/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import type {
   GroupField,
   ProjectReport,
