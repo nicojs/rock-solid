f22191f910834ca54f3b25c29eb32b17d504c4cf
Nico Jansen <jansennico@gmail.com>
2023-12-31 11:56:58 +0100

    Update deployment to include db backup

--- tasks/prepare-deploy.sh
+++ tasks/prepare-deploy.sh
@@ -9,7 +9,7 @@ cp -r ../packages/* ./packages
 rm -rf ./packages/*/node_modules
 cp ../package.json .
 cp ../package-lock.json .
+cp ../import/backup.db backup.db
 npm ci --omit=dev
-mkdir -p ./seed
 npm run -w packages/backend prisma:client:generate
 zip -r deploy.zip .
