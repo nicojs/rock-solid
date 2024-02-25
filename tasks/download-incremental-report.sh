4afa5e64e00f1200e39d610e00269ce56f2e9c0b
Nico Jansen <jansennico@gmail.com>
2024-02-25 17:59:19 +0100

    test(mutation testing): cleanup
    Also add the for-the-badge badge
    
    [![Mutation testing badge](https://img.shields.io/endpoint?style=for-the-badge&url=https%3A%2F%2Fbadge-api.stryker-mutator.io%2Fgithub.com%2Fnicojs%2Frock-solid%2Fmain)](https://dashboard.stryker-mutator.io/reports/github.com/nicojs/rock-solid/main)

--- tasks/download-incremental-report.sh
+++ tasks/download-incremental-report.sh
@@ -3,6 +3,6 @@
 for package in backend
 do
     echo "Downloading $package/main..."
-    curl -s --create-dirs -o packages/$package/reports/stryker-incremental.json https://dashboard.stryker-mutator.io/api/reports/github.com/nicojs/rock-solid/test/add-stryker?module=$package
+    curl -s --create-dirs -o packages/$package/reports/stryker-incremental.json https://dashboard.stryker-mutator.io/api/reports/github.com/nicojs/rock-solid/main?module=$package
 done
 echo 'Done ✅'
