23e6db204ae8a7964dbad0c167671ddcf34ae5f4
Nico Jansen <jansennico@gmail.com>
2024-06-09 20:10:12 +0200

    chore(cleanup): remove old 'seed' scripts (#262)
    As we've been live for about half a year, removing the scripts to seed the database is about time.

--- tasks/import-az2csv.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-az storage file download --share-name import --path import.zip --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
-unzip import.zip -d . 
-rm import.zip
