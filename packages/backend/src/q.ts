fb0dc3bee33d6dafb962a8e396e99ae25665d2a3
Nico Jansen <jansennico@gmail.com>
2025-05-16 21:27:51 +0200

    chore(deps): update dependencies (#296)

---
+++ packages/backend/src/q.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  console.log(`${e.query} ${e.params}`);
+});
+
+const result = await prisma.$queryRaw`SELECT gemeente, COUNT(*) as Aantal
+  FROM Deelname 
+  INNER JOIN Aanmelding ON Aanmelding.Id = Deelname.AanmeldingId AND Aanmelding.Status IN (0, 1) 
+  INNER JOIN Project ON Aanmelding.projectId = Project.id AND Project.type = 1 AND Project.organisatieonderdeel = 2 INNER JOIN Activiteit ON Activiteit.id = Deelname.activiteitId 
+  INNER JOIN Plaats ON Plaats.id = Aanmelding.plaatsId
+  WHERE Deelname.effectieveDeelnamePerunage > 0 AND Activiteit.van BETWEEN ${new Date(2024, 4, 1)} AND ${new Date(2025, 4, 1)}
+  GROUP BY Plaats.gemeente
+`;
+console.log(result);
