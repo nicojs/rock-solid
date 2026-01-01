5d574f3aa552e320167f9da6582c57be2671e009
Nico Jansen <jansennico@gmail.com>
2026-01-01 20:06:53 +0100

    Ontdubbel deelnemers in de vervoerstoer

--- packages/frontend/src/app/projecten/vervoerstoer.component.ts
+++ packages/frontend/src/app/projecten/vervoerstoer.component.ts
@@ -1,7 +1,14 @@
 import { customElement, property, state } from 'lit/decorators.js';
 import { RockElement } from '../rock-element';
 import { css, html, nothing, PropertyValues } from 'lit';
-import { Aanmelding, Locatie, Project, ProjectType } from '@rock-solid/shared';
+import {
+  Aanmelding,
+  Deelnemer,
+  Locatie,
+  notEmpty,
+  Project,
+  ProjectType,
+} from '@rock-solid/shared';
 import { projectService } from './project.service';
 import { deelnemerLink, printProject } from './project.pipes';
 import { fullName } from '../personen/persoon.pipe';
@@ -52,7 +59,12 @@ export class VervoerstoerComponent extends RockElement {
   public aanmeldingen?: Aanmelding[];
 
   @state()
+  private deelnemers: Deelnemer[] = [];
+
+  @state()
   opstapplaatsen: Locatie[] = [];
+
+  aanmeldingenPerDeelnemerId: Map<number, Aanmelding[]> = new Map();
 
   protected override update(
     changedProperties: PropertyValues<VervoerstoerComponent>,
@@ -75,7 +87,24 @@ export class VervoerstoerComponent extends RockElement {
         .filter(
           (opstapplaats) =>
             opstapplaats.geschiktVoorVakantie || !this.enkelVakanties,
+        )
+        .sort((a, b) => a.naam.localeCompare(b.naam));
+      this.deelnemers = this.aanmeldingen
+        .map((aanmelding) => aanmelding.deelnemer)
+        .filter(notEmpty)
+        .filter(
+          (value, index, self) =>
+            value && self.findIndex((val) => val.id === value.id) === index,
         );
+      this.aanmeldingenPerDeelnemerId = new Map();
+      for (const aanmelding of this.aanmeldingen) {
+        if (aanmelding.deelnemerId) {
+          const lijst =
+            this.aanmeldingenPerDeelnemerId.get(aanmelding.deelnemerId) || [];
+          lijst.push(aanmelding);
+          this.aanmeldingenPerDeelnemerId.set(aanmelding.deelnemerId, lijst);
+        }
+      }
     }
     super.update(changedProperties);
   }
@@ -105,13 +134,12 @@ export class VervoerstoerComponent extends RockElement {
     if (!this.aanmeldingen) return;
     this.isLoading = true;
     await Promise.all(
-      this.aanmeldingen
-        .map(async (aanmelding) => {
-          if (aanmelding.deelnemer) {
-            await persoonService.patchPersoon(aanmelding.deelnemer.id, {
-              id: aanmelding.deelnemer.id,
-              mogelijkeOpstapplaatsen:
-                aanmelding.deelnemer.mogelijkeOpstapplaatsen,
+      this.deelnemers
+        .map(async (deelnemer) => {
+          if (deelnemer) {
+            await persoonService.patchPersoon(deelnemer.id, {
+              id: deelnemer.id,
+              mogelijkeOpstapplaatsen: deelnemer.mogelijkeOpstapplaatsen,
             });
           }
         })
@@ -147,6 +175,37 @@ export class VervoerstoerComponent extends RockElement {
             >`,
         )}
       </p>
+      <details class="alert alert-info">
+        <summary>Uitleg</summary>
+        <p>
+          Hieronder kun je mogelijke opstapplaatsen van de deelnemers beheren en
+          een opstapplaats toewijzen per deelnemer.
+        </p>
+        <ol>
+          <li>
+            Gebruik het veld 'Opstapplaatsen' om beschikbare opstapplaatsen toe
+            te voegen aan de vervoerstoer. Maak nieuwe opstapplaatsen aan via
+            <a href="/locaties">Locaties beheren</a>
+          </li>
+          <li>
+            Voor elke deelnemer in de tabel kun je de opstapplaatsen beheren door op de icoontjes te klikken.
+            <ul>
+              <li>
+                <rock-icon icon="dashCircle"></rock-icon> Geen mogelijke
+                opstapplaats.
+              </li>
+              <li>
+                <rock-icon icon="circle"></rock-icon> Mogelijke opstapplaats.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Gebruik de dropdown in de laatste kolom om een opstapplaats te
+            kiezen voor deze vervoerstoer.
+          </li>
+          <li>Klik op 'Opslaan' om je wijzigingen te bewaren.</li>
+        </ol>
+      </details>
       <div class="row mb-3 z-index-9999">
         <div class="col">
           <rock-reactive-form-tags
@@ -167,104 +226,127 @@ export class VervoerstoerComponent extends RockElement {
               },
               { label: 'Opstapplaatsen' },
             )}
-          ></rock-reactive-form-tags>
+          >
+          </rock-reactive-form-tags>
         </div>
       </div>
-      <div class="row mb-3">
-        ${this.aanmeldingen.length === 0
-          ? html`<p class="alert alert-info">
-              Deze projecten hebben nog geen deelnemers met status 'Aangemeld'
-              of 'Bevestigd'.
-            </p>`
-          : this.aanmeldingen.every(
-                (aanmelding) =>
-                  !aanmelding.deelnemer ||
-                  aanmelding.deelnemer?.mogelijkeOpstapplaatsen.length === 0,
-              )
+      <div class="row">
+        <div class="col mb-3">
+          <div class="d-flex gap-3 align-items-center">
+            <strong>Legenda:</strong>
+            <span>
+              <rock-icon icon="checkCircle" class="text-success"></rock-icon>
+              Gekozen opstapplaats
+            </span>
+            <span>
+              <rock-icon icon="circle"></rock-icon>
+              Mogelijke opstapplaats
+            </span>
+            <span>
+              <rock-icon icon="dashCircle"></rock-icon>
+              Geen mogelijke opstapplaats
+            </span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          ${this.deelnemers.length === 0
             ? html`<p class="alert alert-info">
-                Deelnemers in deze projecten hebben nog geen mogelijke
-                opstapplaatsen.
+                Deze projecten hebben nog geen deelnemers met status 'Aangemeld'
+                of 'Bevestigd'.
               </p>`
-            : nothing}
-        <table class="table table-hover table-sm table-responsive">
-          <thead class="sticky-top z-index-900">
-            <tr>
-              <th style="width: 200px">Deelnemer</th>
-              ${this.opstapplaatsen?.map(
-                (opstapplaats) =>
-                  html`<th style="width: 40px" class="text-vertical">
-                    ${opstapplaats.naam}
-                  </th>`,
-              )}
-              <th>Gekozen opstapplaats</th>
-            </tr>
-          </thead>
-          <tbody>
-            ${this.aanmeldingen.map((aanmelding) => {
-              const deelnemer = aanmelding.deelnemer;
-              if (!deelnemer) return nothing;
-              return html`
+            : this.deelnemers.every(
+                  (deelnemer) => deelnemer.mogelijkeOpstapplaatsen.length === 0,
+                )
+              ? html`<p class="alert alert-info">
+                  Deelnemers in deze projecten hebben nog geen mogelijke
+                  opstapplaatsen.
+                </p>`
+              : nothing}
+
+          <div class="col-12 table-responsive">
+            <table class="table table-hover table-sm">
+              <thead class="sticky-top z-index-900">
                 <tr>
-                  <th class="fw-normal">${deelnemerLink(deelnemer)}</th>
-                  ${this.opstapplaatsen.map((opstapplaats) =>
-                    this.renderOpstapplaatsCell(aanmelding, opstapplaats),
+                  <th style="width: 200px">Deelnemer</th>
+                  ${this.opstapplaatsen?.map(
+                    (opstapplaats) =>
+                      html`<th style="width: 40px" class="text-vertical">
+                        ${opstapplaats.naam}
+                      </th>`,
                   )}
-                  <td>
-                    <select
-                      class="form-select form-select-sm"
-                      @change=${(ev: Event) => {
-                        const select = ev.target as HTMLSelectElement;
-                        const selectedId = Number(select.value);
-                        const geselecteerdeLocatie =
-                          deelnemer.mogelijkeOpstapplaatsen.find(
-                            (loc) => loc.id === selectedId,
-                          );
-                        aanmelding.opstapplaats = geselecteerdeLocatie;
-                        this.requestUpdate();
-                      }}
-                    >
-                      <option value="">Kies opstapplaats</option>
-                      ${deelnemer.mogelijkeOpstapplaatsen.map(
-                        (locatie) =>
-                          html`<option
-                            value="${locatie.id}"
-                            ?selected=${aanmelding.opstapplaats?.id ===
-                            locatie.id}
-                          >
-                            ${showLocatie(locatie)}
-                          </option>`,
-                      )}
-                    </select>
-                  </td>
+                  <th>Gekozen opstapplaats</th>
                 </tr>
-              `;
-            })}
-          </tbody>
-        </table>
-        <div class="col">
-          <button
-            class="btn btn-primary"
-            @click=${() => this.save()}
-            ?disabled=${this.isLoading}
-          >
-            ${this.isLoading
-              ? html`<span
-                    class="spinner-border spinner-border-sm"
-                    aria-hidden="true"
-                  ></span>
-                  <span role="status">Opslaan...</span>`
-              : html`<rock-icon icon="floppy"></rock-icon> Opslaan`}
-          </button>
+              </thead>
+              <tbody>
+                ${this.deelnemers.map((deelnemer) => {
+                  if (!deelnemer) return nothing;
+                  return html`
+                    <tr>
+                      <th class="fw-normal">${deelnemerLink(deelnemer)}</th>
+                      ${this.opstapplaatsen.map((opstapplaats) =>
+                        this.renderOpstapplaatsCell(deelnemer, opstapplaats),
+                      )}
+                      <td>
+                        <select
+                          class="form-select form-select-sm"
+                          @change=${(ev: Event) => {
+                            const select = ev.target as HTMLSelectElement;
+                            const selectedId = Number(select.value);
+                            const geselecteerdeLocatie =
+                              deelnemer.mogelijkeOpstapplaatsen.find(
+                                (loc) => loc.id === selectedId,
+                              );
+                            const aanmeldingen =
+                              this.aanmeldingenPerDeelnemerId.get(
+                                deelnemer.id,
+                              ) || [];
+                            for (const aanmelding of aanmeldingen) {
+                              aanmelding.opstapplaats = geselecteerdeLocatie;
+                            }
+                            this.requestUpdate();
+                          }}
+                        >
+                          <option value="">Kies opstapplaats</option>
+                          ${deelnemer.mogelijkeOpstapplaatsen.map(
+                            (locatie) =>
+                              html`<option
+                                value="${locatie.id}"
+                                ?selected=${this.aanmeldingenPerDeelnemerId.get(
+                                  deelnemer.id,
+                                )?.[0]?.opstapplaats?.id === locatie.id}
+                              >
+                                ${showLocatie(locatie)}
+                              </option>`,
+                          )}
+                        </select>
+                      </td>
+                    </tr>
+                  `;
+                })}
+              </tbody>
+            </table>
+          </div>
+          <div class="col">
+            <button
+              class="btn btn-primary"
+              @click=${() => this.save()}
+              ?disabled=${this.isLoading}
+            >
+              ${this.isLoading
+                ? html`<span
+                      class="spinner-border spinner-border-sm"
+                      aria-hidden="true"
+                    ></span>
+                    <span role="status">Opslaan...</span>`
+                : html`<rock-icon icon="floppy"></rock-icon> Opslaan`}
+            </button>
+          </div>
         </div>
       </div>
     `;
   }
 
-  private renderOpstapplaatsCell(
-    aanmelding: Aanmelding,
-    opstapplaats: Locatie,
-  ) {
-    const deelnemer = aanmelding.deelnemer!;
+  private renderOpstapplaatsCell(deelnemer: Deelnemer, opstapplaats: Locatie) {
     const naam = fullName(deelnemer);
     const isOpstapplaats =
       deelnemer.mogelijkeOpstapplaatsen.find(
@@ -286,13 +368,19 @@ export class VervoerstoerComponent extends RockElement {
                 ),
                 1,
               );
-              if (aanmelding.opstapplaats?.id === opstapplaats.id) {
-                aanmelding.opstapplaats = undefined;
-              }
+              this.aanmeldingenPerDeelnemerId
+                .get(deelnemer.id)
+                ?.forEach((aanmelding) => {
+                  if (aanmelding.opstapplaats?.id === opstapplaats.id) {
+                    aanmelding.opstapplaats = undefined;
+                  }
+                });
               this.requestUpdate();
             }}
           >
-            ${opstapplaats.id === aanmelding.opstapplaats?.id
+            ${opstapplaats.id ===
+            this.aanmeldingenPerDeelnemerId.get(deelnemer.id)?.[0]?.opstapplaats
+              ?.id
               ? html`<rock-icon
                   icon="checkCircle"
                   class="text-success"
