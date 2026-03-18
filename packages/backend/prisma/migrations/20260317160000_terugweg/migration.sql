78c401f341f3d0d762b1037a31afeaccebe1bd24
Nico Jansen <jansennico@gmail.com>
2026-03-18 23:04:38 +0100

    Squash migrations

--- packages/backend/prisma/migrations/20260317160000_terugweg/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable Vervoerstoer
-ALTER TABLE "Vervoerstoer" ADD COLUMN "datum" DATETIME;
-ALTER TABLE "Vervoerstoer" ADD COLUMN "datumTerug" DATETIME;
-
--- AlterTable VervoerstoerRoute
-ALTER TABLE "VervoerstoerRoute" ADD COLUMN "vertrekTijdTerug" DATETIME;
-
--- AlterTable VervoerstoerStop
-ALTER TABLE "VervoerstoerStop" ADD COLUMN "geplandeAankomstTerug" DATETIME;
